77dc78df1ce1789cbf3de4d8df07ed53568b4581
altitude <altitude@users.noreply.github.com>
2022-08-02 10:45:13 +0000

    Upgrade Release to v1.7.1

--- ledgerclient/apis/__init__.py
+++ ledgerclient/apis/__init__.py
@@ -6,7 +6,7 @@
 # raise a `RecursionError`.
 # In order to avoid this, import only the API that you directly need like:
 #
-#   from .api.accounts_api import AccountsApi
+#   from ledgerclient.api.accounts_api import AccountsApi
 #
 # or import this package, but before doing it, use:
 #
