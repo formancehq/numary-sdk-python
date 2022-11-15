21758f723f9d8c449393df83b1b56e503b535898
flemzord <flemzord@users.noreply.github.com>
2022-11-15 14:01:52 +0000

    Upgrade Release to v1.7.8

---
+++ ledgerclient/apis/paths/ledger_mapping.py
@@ -0,0 +1,9 @@
+from ledgerclient.paths.ledger_mapping.get import ApiForget
+from ledgerclient.paths.ledger_mapping.put import ApiForput
+
+
+class LedgerMapping(
+    ApiForget,
+    ApiForput,
+):
+    pass
---
+++ ledgerclient/apis/paths/ledger_transactions_batch.py
@@ -0,0 +1,7 @@
+from ledgerclient.paths.ledger_transactions_batch.post import ApiForpost
+
+
+class LedgerTransactionsBatch(
+    ApiForpost,
+):
+    pass
